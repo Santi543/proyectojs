d5cbd1625a38b2c920ddb31312e8a054a1b41747
Santi543 <santioldani08@gmail.com>
2022-09-22 19:03:49 -0300

    adding modals

--- index.js
+++ index.js
@@ -1,15 +1,82 @@
-/* let userName;
-do {
-    userName = prompt(`Ingrese su mail, por favor`);
-} while (!userName || !userName.includes(`@`));
+const userInput = document.getElementById(`exampleInputEmail1`)
+const userPass = document.getElementById(`exampleInputPassword1`)
+const loginInput = document.getElementById(`exampleInputEmail2`)
+const loginPass = document.getElementById(`exampleInputPassword2`)
 
-const user = userName.split(`@`);
+let userNav = JSON.parse(localStorage.getItem("Users"))
 
-alert(`Bienvenido, ${user[0]}.\n\nSi desea ver nuestros productos haga click en "ELEGIR PRODUCTOS".`); */
+
+
+/* alert(`Bienvenido, ${userNav[0]}.\n\nSi desea ver nuestros productos haga click en "ELEGIR PRODUCTOS".`); */
+
+const addToNav = () => {
+    /* let navVar = `<p class="userNav> ${userNav[0]}`
+    document.getElementById(``).innerHTML = navVar */
+    if (userNav) {
+        console.log(userNav)
+        console.log(userNav.mail.split(`@`))
+        document.getElementById(`userName`).innerHTML = `<p> ${userNav.mail.split(`@`)[0]} </p>`
+
+    }
+}
+
+
+const register = document.getElementById(`register`)
+register.addEventListener(`click`, (e) => {
+    if (userInput.value.includes(`@`) && userInput.value != null) {
+        const userObj = { mail: userInput.value, password: userPass.value }
+        localStorage.setItem(`Users`, JSON.stringify(userObj))
+        registerNav.style.display = 'none';
+    } else {
+        alert(`Asegurese que sea un mail`)
+    }
+})
+
+const registerNav = document.getElementById(`btnRegisterNav`)
+
+
+const goRegister = document.getElementById(`goToRegister`)
+
+goRegister.addEventListener(`click`, (e) => {
+    $('#exampleModalCenter2').modal('hide')
+})
+
+const addToModal = () => {
+    let modalVar = ``
+    cartProduct.map((product) => {
+        modalVar = `${modalVar} <div class="containerProduct"> <img src= "./images/${product.imagen}" class="modalVar">  <p class="textModal"> ${product.nameProduct}    x ${product.cantidad} </p>  </div>`
+    })
+    document.getElementById(`modalCartImg`).innerHTML = modalVar
+}
+
+
+
+const cartImg = document.getElementById(`cartImg`)
+
+cartImg.addEventListener(`click`, (e) => {
+    addToModal()
+})
+
+
+
+const login = document.getElementById(`login`)
+login.addEventListener(`click`, (e) => {
+    let loginObj = localStorage.getItem(`Users`)
+    loginObj = JSON.parse(loginObj)
+    if ((loginObj.mail == loginInput.value) && (loginObj.password == loginPass.value)) {
+        alert(`Usted se ha logeado correctamente`)
+        localStorage.setItem(`token`, `ariziochan`)
+        addToNav()
+    } else {
+        alert(`Hay algo incorrecto. Vuelva a logearse.`)
+    }
+})
 
 const multiplicar = (cantidad, precio) => cantidad * precio
 
 let cartProduct = []
+
+cartProduct = localStorage.getItem(`cart`) ? JSON.parse(localStorage.getItem(`cart`)) : []
 
 let precioFinal = 0
 cartProduct.forEach(producto => {
@@ -24,61 +91,62 @@ const actContador = () => {
     cart.textContent = contador;
 }
 
-const caseFunction = (nameProduct, cantidad, total) => {
-    alert(`Usted ha seleccionado ${cantidad} ${nameProduct} \n Total: $${total}`);
-    cartProduct.push({ cantidad: cantidad, producto: nameProduct, total: total })
-    console.log(cartProduct)
-}
+const addNew = (nameProduct, total, imagen) => {
+    if (localStorage.getItem(`token`) == `ariziochan`) {
+        let newCart = []
+        if (cartProduct.length > 0) {
+           newCart = cartProduct.map((item) => {
+                if (nameProduct == item.nameProduct) {
+                    return { ...item, cantidad: item.cantidad + 1, total: item.total + total }
+                } else {
+                    return item;
+                }
 
-const addNew = (nameProduct, total) => {
-
-const newCart = cartProduct.map((item) => {
-    if(nameProduct == item.nameProduct){
-        return {...item , cantidad: item.cantidad + 1}
+            })
+        }
+        if (!cartProduct.find((item) => item.nameProduct == nameProduct)) {
+            newCart.push({ cantidad: 1, nameProduct: nameProduct, total: total, imagen: imagen })
+        }
+        localStorage.setItem(`cart`, JSON.stringify(newCart))
+        return newCart;
     } else {
-        return item;
+        $('#exampleModalCenter2').modal({ show: true });
     }
-})
-    if (!cartProduct.find((item) => item.nameProduct == nameProduct)){
-    newCart.push({ cantidad: 1, nameProduct: nameProduct, total: total })
-    }
-    return newCart;
 }
 
 
 
 
 const comprarProducto = (selectProduct) => {
-
+    console.log(cartProduct)
     switch (selectProduct) {
 
         case 1:
-            cartProduct = addNew(`HAUTEVILLE CONCRETE ROCKING CHAIR`, 350);
-            console.log(cartProduct)
+            cartProduct = addNew(`HAUTEVILLE CONCRETE ROCKING CHAIR`, 350, "product-1.jpg");
             break;
         case 2:
-            cartProduct = addNew("PAVILION SPEAKER", 600);
+            cartProduct = addNew("PAVILION SPEAKER", 600, "product-2.jpg");
             break;
         case 3:
-            cartProduct = addNew("LIGOMANCER", 780);
+            cartProduct = addNew("LIGOMANCER", 780, "product-3.jpg");
             break;
         case 4:
-            cartProduct = addNew("ALATO CABINET", 800);
+            cartProduct = addNew("ALATO CABINET", 800, "product-4.jpg");
             break;
         case 5:
-            cartProduct = addNew("EARING WIRELESS", 100);
+            cartProduct = addNew("EARING WIRELESS", 100, "product-5.jpg");
             break;
         case 6:
-            cartProduct = addNew("SCUPTURAL COFFEE TABLE", 960);
+            cartProduct = addNew("SCUPTURAL COFFEE TABLE", 960, "product-6.jpg");
             break;
         case 7:
-            cartProduct = addNew("THE WW CHAIR", 540);
+            cartProduct = addNew("THE WW CHAIR", 540, "product-7.jpg");
             break;
         case 8:
-            cartProduct = addNew("HIMITSU MONEY BOX", 55);
+            cartProduct = addNew("HIMITSU MONEY BOX", 55, "product-8.jpg");
             break;
         case 9:
-            cartProduct = addNew("ARIANE PRIN", 99);
+            cartProduct = addNew("ARIANE PRIN", 99, "product-9.jpg");
             break;
     }
     let precioFinal = 0
@@ -92,6 +160,7 @@ const shoppingButtons = document.getElementsByClassName(`shoppingButtons`)
 console.log(shoppingButtons)
 for (var i = 0; i < shoppingButtons.length; i++) {
     shoppingButtons[i].addEventListener('click', (e) => {
+        console.log(`hola`)
         comprarProducto(Number(e.target.id))
         console.log(e.target.id)
         contador++
