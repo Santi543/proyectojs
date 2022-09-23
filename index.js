435eb9ca7ac3ec9ed4c6ee5a4620d37bc60859a4
Santi543 <santioldani08@gmail.com>
2022-09-23 19:10:26 -0300

    deleteProduct

--- index.js
+++ index.js
@@ -44,11 +44,30 @@ goRegister.addEventListener(`click`, (e) => {
 const addToModal = () => {
     let modalVar = ``
     cartProduct.map((product) => {
-        modalVar = `${modalVar} <div class="containerProduct"> <img src= "./images/${product.imagen}" class="modalVar">  <p class="textModal"> ${product.nameProduct}    x ${product.cantidad} </p>  </div>`
+        modalVar = `${modalVar}
+        <div class="containerProduct">
+         <img src= "./images/${product.imagen}" class="modalVar">
+            <div class= "contModalCart">
+                <p class="textModal"> ${product.nameProduct}    x ${product.cantidad}
+                </p>
+            </div>
+                <button type="button" class="close buttonDlt" id="${product.nameProduct}">
+				<span>&times;</span>
+			    </button>
+        </div>`
     })
     document.getElementById(`modalCartImg`).innerHTML = modalVar
 }
 
+const btnDlt = document.getElementsByClassName(`buttonDlt`)
+console.log(btnDlt)
+if (btnDlt.length > 0){
+btnDlt?.map((product) => {
+    product.addEventListener(`click`, (e) =>{
+        console.log(e.id)
+    })
+})
+}
 
 
 const cartImg = document.getElementById(`cartImg`)
@@ -91,7 +110,7 @@ const actContador = () => {
     cart.textContent = contador;
 }
 
-const addNew = (nameProduct, total, imagen) => {
+const addNew = (nameProduct, total, imagen, id) => {
     if (localStorage.getItem(`token`) == `ariziochan`) {
         let newCart = []
         if (cartProduct.length > 0) {
@@ -105,7 +124,7 @@ const addNew = (nameProduct, total, imagen) => {
             })
         }
         if (!cartProduct.find((item) => item.nameProduct == nameProduct)) {
-            newCart.push({ cantidad: 1, nameProduct: nameProduct, total: total, imagen: imagen })
+            newCart.push({ cantidad: 1, nameProduct: nameProduct, total: total, imagen: imagen, id: id })
         }
         localStorage.setItem(`cart`, JSON.stringify(newCart))
         return newCart;
@@ -122,31 +141,31 @@ const comprarProducto = (selectProduct) => {
     switch (selectProduct) {
 
         case 1:
-            cartProduct = addNew(`HAUTEVILLE CONCRETE ROCKING CHAIR`, 350, "product-1.jpg");
+            cartProduct = addNew(`HAUTEVILLE CONCRETE ROCKING CHAIR`, 350, "product-1.jpg", 1);
             break;
         case 2:
-            cartProduct = addNew("PAVILION SPEAKER", 600, "product-2.jpg");
+            cartProduct = addNew("PAVILION SPEAKER", 600, "product-2.jpg", 2);
             break;
         case 3:
-            cartProduct = addNew("LIGOMANCER", 780, "product-3.jpg");
+            cartProduct = addNew("LIGOMANCER", 780, "product-3.jpg", 3);
             break;
         case 4:
-            cartProduct = addNew("ALATO CABINET", 800, "product-4.jpg");
+            cartProduct = addNew("ALATO CABINET", 800, "product-4.jpg", 4);
             break;
         case 5:
-            cartProduct = addNew("EARING WIRELESS", 100, "product-5.jpg");
+            cartProduct = addNew("EARING WIRELESS", 100, "product-5.jpg", 5);
             break;
         case 6:
-            cartProduct = addNew("SCUPTURAL COFFEE TABLE", 960, "product-6.jpg");
+            cartProduct = addNew("SCUPTURAL COFFEE TABLE", 960, "product-6.jpg", 6);
             break;
         case 7:
-            cartProduct = addNew("THE WW CHAIR", 540, "product-7.jpg");
+            cartProduct = addNew("THE WW CHAIR", 540, "product-7.jpg", 7);
             break;
         case 8:
-            cartProduct = addNew("HIMITSU MONEY BOX", 55, "product-8.jpg");
+            cartProduct = addNew("HIMITSU MONEY BOX", 55, "product-8.jpg", 8);
             break;
         case 9:
-            cartProduct = addNew("ARIANE PRIN", 99, "product-9.jpg");
+            cartProduct = addNew("ARIANE PRIN", 99, "product-9.jpg", 9);
             break;
     }
     let precioFinal = 0
